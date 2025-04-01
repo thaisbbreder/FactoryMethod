a6c311ab6ecf676f5051abb1a943698a143460d9
Thaís Breder <thaisbragabreder@gmail.com>
2025-04-01 09:14:38 -0300

    removing comments

--- src/AfterFactoryMethod/Notification.java
+++ src/AfterFactoryMethod/Notification.java
@@ -1,9 +1,5 @@
 package AfterFactoryMethod;
 
-//Interface com um método genérico enviar(), que todas as notificações devem implementar.
-//Garante que todas as notificações tenham o mesmo comportamento e facilita a adicionar novos tipos de notificações no futuro */
-
-// Interface para Notificação
 interface Notification {
     void notifyUser();
 }
